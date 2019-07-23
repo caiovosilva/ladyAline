4a9382c01a58558461e9aa2b52f2e5ea6a7c5dd5
Caio Silva <caiovosilva@outlook.com>
2019-07-23 11:56:06 -0300

    spotify's login and ~now playing working

---
+++ app/ClientApp/src/components/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "03b1245bd14a4240a24f2061f3354fde";
+export const redirectUri = "https://localhost:5001";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
