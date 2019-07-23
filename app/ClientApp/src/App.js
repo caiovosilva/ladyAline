9d5d937927d1e83be92ccf29aab3199a7ff96cbc
Caio Silva <caiovosilva@outlook.com>
2019-07-23 01:41:47 -0300

    comecei a implementar o cunsumo da API do spotify

--- app/ClientApp/src/App.js
+++ app/ClientApp/src/App.js
@@ -5,13 +5,95 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
+import hash from "./hash";
+import logo from "./logo.svg";
+import "./App.css";
+import * as $ from "jquery";
+import Player from "./Player";
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "03b1245bd14a4240a24f2061f3354fde";
+const redirectUri = "https://localhost:5001/";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+
+
 export default class App extends Component {
   static displayName = App.name;
+
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+    item: {
+      album: {
+        images: [{ url: "" }]
+      },
+      name: "",
+      artists: [{ name: "" }],
+      duration_ms:0,
+    },
+    is_playing: "Paused",
+    progress_ms: 0
+  };  
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+  }  
+  
+  getCurrentlyPlaying(token) {
+    // Make a call using the token
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: (data) => {
+        this.setState({
+          item: data.item,
+          is_playing: data.is_playing,
+          progress_ms: data.progress_ms,
+        });
+      }
+    });
+  }
+
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+    }
+  }
 
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
+        <div className="App">
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {!this.state.token && (
+          <a
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+          >
+            Login to Spotify
+          </a>
+        )}
+        {this.state.token && (
+          <Player
+            item={this.state.item}
+            is_playing={this.state.is_playing}
+            progress_ms={this.progress_ms}
+          />
+        )}
+        </header>
+      </div>
+        {/*<Route exact path='/' component={Home} />*/}
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
       </Layout>
