f0d24eb8cadce2613cdc6e3bea9381e1597616b4
Caio Silva <caiovosilva@outlook.com>
2019-07-25 22:02:04 -0300

    habilitada a edicao de artistas favoritos

--- app/ClientApp/src/components/FavoriteArtists.js
+++ app/ClientApp/src/components/FavoriteArtists.js
@@ -11,14 +11,22 @@ export class FavoriteArtists extends Component {
       artists: [],
       loading: true,
       newArtistModal: false,
-      newArtistName: ''
+      newArtistName: '',
+      editArtistModal: false,
+      editArtistData: {
+        id:0,
+        name:''
+      }
     };
+    this.fillTable();
+  }
 
+  fillTable(){
     fetch('api/FavoriteArtists')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ artists: data, loading: false });
-      });
+    .then(response => response.json())
+    .then(data => {
+      this.setState({ artists: data, loading: false });
+    });
   }
 
   renderArtistsTable () {
@@ -37,7 +45,7 @@ export class FavoriteArtists extends Component {
               <td>{artist.id}</td>
               <td>{artist.name}</td>
               <td>
-                <Button color='success' className='mr-2'>Editar</Button>
+                <Button color='success' className='mr-2' onClick={this.toggleEditFavoriteArtistModal.bind(this, artist.id, artist.name)}>Editar</Button>
                 <Button color="danger">Excluir</Button>
               </td>
             </tr>
@@ -48,18 +56,51 @@ export class FavoriteArtists extends Component {
     );
   }
 
+  editArtist() {
+    return (
+      <div>
+      <Modal isOpen={this.state.editArtistModal} toggle={this.toggleEditFavoriteArtistModal.bind(this)}>
+        <ModalHeader toggle={this.toggleEditFavoriteArtistModal.bind(this)}>Editar Artista Favorito</ModalHeader>
+        <ModalBody>
+          <Input value={this.state.editArtistData.name} onChange={(e) => {
+            let editArtistData = this.state;
+            editArtistData.name = e.target.value;
+            this.setState({editArtistData});
+          }}/>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={this.updateArtist.bind(this)}>Salvar</Button>{' '}
+          <Button color="secondary" onClick={this.toggleEditFavoriteArtistModal.bind(this)}>Cancelar</Button>
+        </ModalFooter>
+      </Modal>
+      </div>
+    );
+  }
+
   toggleNewFavoriteArtistModal() {
     this.setState({
       newArtistModal: !this.state.newArtistModal
     });
   }
 
-  renderArtistsModal(){
+  toggleEditFavoriteArtistModal(id, name) {
+    if(name!==undefined){
+      let editArtistData = this.state;
+      editArtistData.name = name;
+      editArtistData.id = id;
+      this.setState({editArtistData});
+    }
+    this.setState({
+      editArtistModal: !this.state.editArtistModal
+    });
+  }
+
+  renderNewArtistsModal(){
     return (
       <div>
       <Button color="primary" onClick={this.toggleNewFavoriteArtistModal.bind(this)}>Adicionar Novo Artista Favorito</Button>
       <Modal isOpen={this.state.newArtistModal} toggle={this.toggleNewFavoriteArtistModal.bind(this)}>
-        <ModalHeader toggle={this.toggleNewFavoriteArtistModal.bind(this)}>Adicionar Novo Artista Favorito</ModalHeader>
+        <ModalHeader toggle={this.toggleNewFavoriteArtistModal.bind(this)}>Artista Favorito</ModalHeader>
         <ModalBody>
           <Input placeholder="Nome do Artista" value={this.state.newArtistName} onChange={(e) => {
             let newArtistName = this.state.newArtistName;
@@ -91,15 +132,36 @@ export class FavoriteArtists extends Component {
     });
   }
 
+  updateArtist(){
+    
+    const artist = {
+      id: this.state.editArtistData.id,
+      name: this.state.editArtistData.name
+    };
+    axios.put('api/FavoriteArtists/'+this.state.editArtistData.id, artist).then((response) => {
+      //nao eh o ideal, se tiver tempo eu vejo como atualizar apenas um item da lista
+      this.fillTable();
+      this.setState({
+        editArtistModal: false,
+        editArtistData: {
+          id: 0,
+          name:''
+        }
+      });
+    });
+  }
+
   render () {
     let contents = this.state.loading
       ? <p><em>Loading...</em></p>
       : this.renderArtistsTable();
-    let modal = this.renderArtistsModal();
+    let newArtmodal = this.renderNewArtistsModal();
+    let editArtistModal = this.editArtist();
     return (
       <div>
         <h1>Aqui está a lista de seus artistas favoritos!</h1>
-        {modal}
+        {editArtistModal}
+        {newArtmodal}
         {contents}
       </div>
     );
